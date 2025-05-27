65078371abce1ed1fabfe01dc3ca1246e8d02d16
AB <174193320+FrankTheSssnake@users.noreply.github.com>
2025-05-27 12:30:58 +0530

    Implemented File Header Logic

--- sms/src/extra.rs
+++ sms/src/extra.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 use std::fs::File;
 use std::cell::RefCell;
 
@@ -16,8 +17,34 @@ fn get_pattern() -> String {}
 
 pub fn calculate_widths(header: &String) {}
 
-fn format_header() -> String {}
+fn format_header(widths: &[usize; NO_OF_COLUMNS]) -> String {
+    
+    let mut output: String = String::new();
 
-fn format_seperator() -> String {}
+    for i in 0..NO_OF_COLUMNS {
+        output.push_str(format!("{:<width$} | ", HEADERS[i], width = widths[i]).as_str());
+    }
 
-pub fn fprint_head(file: &mut File) {}
+    output.truncate(output.len() - 3);
+
+    output
+}
+
+fn format_seperator(widths: &[usize; NO_OF_COLUMNS]) -> String {
+
+    let mut total_witdh: i32 = widths.iter().sum::<usize>() as i32;
+    total_witdh += 3 * (NO_OF_COLUMNS as i32 - 1);
+
+    "-".repeat(total_witdh as usize)
+}
+
+pub fn fprint_head(file: &mut File) {
+
+    let widths: [usize; NO_OF_COLUMNS] = WIDTHS.with(|w| *w.borrow());
+
+    let header: String = format_header(&widths);
+    let seperator: String = format_seperator(&widths);
+
+    write!(file, "{}\n{}\n", header, seperator);
+
+}
