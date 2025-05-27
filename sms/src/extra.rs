13dea5f54f48e2bc9572619ce766d576cce6513d
AB <174193320+FrankTheSssnake@users.noreply.github.com>
2025-05-27 11:59:02 +0530

    Refined shared declarations

--- sms/src/extra.rs
+++ sms/src/extra.rs
@@ -1,10 +1,13 @@
 use std::fs::File;
+use std::cell::RefCell;
 
+pub const NO_OF_COLUMNS: usize = 3;
 
-const NO_OF_COLUMNS: usize = 3;
-
-pub static mut WIDTHS: [i32; NO_OF_COLUMNS] = [5, 5, 5];
 static HEADERS: [&str; NO_OF_COLUMNS] = ["ID", "Name", "Marks"];
+
+thread_local! {
+    pub static WIDTHS: RefCell<[usize; NO_OF_COLUMNS]> = RefCell::new([5, 5, 5]);
+}
 
 
 // TODO
